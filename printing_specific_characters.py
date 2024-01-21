44a7a15d12ab4760363548c9ac6437a31af61c2e
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-21 20:32:28 +0700

    second trial

--- printing_specific_characters.py
+++ printing_specific_characters.py
@@ -1,11 +1,13 @@
 # Accept a string input from the user
+print("Here are the only even index char:")
 user_input = input("Please enter the word: ")
 
 # Display characters at even index numbers
 result = ""
+print("Here are the only even index char:")
 for index in range(0, len(user_input), 2):
     result += user_input[index]
     
 # Print the result
-    print(f"Here are the only even letters:", result)
+    print(result)
 
